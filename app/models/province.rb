12f33dac755f42ca94ad603479eb7b1f378140db
Mauro Almiron <mdavid.almiron@gmail.com>
2024-11-13 23:51:40 -0300

    fix spec & add cities relation

--- app/models/province.rb
+++ app/models/province.rb
@@ -1,5 +1,6 @@
 class Province < ApplicationRecord
+  has_many :cities
   validates :name, presence: true,
-    uniqueness: { case_sensitive: false, message: "Ya existe una provincia registrada con este nombre" }
+    uniqueness: { case_sensitive: true, message: "Ya existe una provincia registrada con este nombre" }
   scope :actives, -> { where(active: true) }
 end
--- spec/models/province_spec.rb
+++ spec/models/province_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe Province, type: :model do
   subject { build(:province) }
   describe "validations" do
     it { is_expected.to validate_presence_of(:name) }
-    it { is_expected.to validate_uniqueness_of(:name).case_insensitive.with_message("Ya existe una provincia registrada con este nombre") }
+    it { is_expected.to validate_uniqueness_of(:name).with_message("Ya existe una provincia registrada con este nombre") }
   end
 end
