7ee78decdaae2eba30b16f3c5d413d51aba39a10
Mauro Almiron <mdavid.almiron@gmail.com>
2024-11-14 20:48:54 -0300

    ✅fix system test: with this test I ensure that my row has equal attributes like the table needs

--- spec/system/company_categories.rb
+++ spec/system/company_categories.rb
@@ -8,16 +8,16 @@ RSpec.describe "CompanyCategories", type: :system do
     expect(trs.size).to eq(2)
 
     tbody = find("tbody")
+    ths = find_all("th"
     within tbody do
       tds = find_all("td")
-      ths = find_all("th")
-      expect(tds.size).to eq(tds.size)
+      expect(tds.size).to eq(ths.size)
     end
   end
 
   it "show form in modal" do
     visit company_categories_path
     click_on "Registrar categoría"
-    expect(page).to have_css("h4", text:"Nueva categoría")
+    expect(page).to have_css("h4", text: "Nueva categoría")
   end
 end
