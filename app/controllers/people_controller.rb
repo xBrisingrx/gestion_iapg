117634438e68ec5f1f0087bc9b8ebe3fe481b845
Mauro Almiron <mdavid.almiron@gmail.com>
2024-11-19 00:20:16 -0300

    add pagy to people table

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,7 +3,7 @@ class PeopleController < ApplicationController
 
   # GET /people or /people.json
   def index
-    @people = Person.all
+    @pagy, @people = pagy(Person.actives)
   end
 
   # GET /people/1 or /people/1.json
