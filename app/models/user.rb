600ca657e9510a2a96b7744858b2e7aeacf30a7b
Mauro Almiron <mdavid.almiron@gmail.com>
2024-11-19 12:36:36 -0300

    change user: add name, role and username

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,10 +13,11 @@ class User < ApplicationRecord
   has_many :sessions, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :password, allow_nil: true, length: { minimum: 12 }
+  validates :password, allow_nil: true, length: { minimum: 6 }
 
   normalizes :email, with: -> { _1.strip.downcase }
 
+  enum :role, [ :admin, :editor, :guest ]
   before_validation if: :email_changed?, on: :update do
     self.verified = false
   end
--- config/routes.rb
+++ config/routes.rb
@@ -27,5 +27,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "main#index"
+  # root "main#index"
 end
