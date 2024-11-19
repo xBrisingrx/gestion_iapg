2a864523849aaa43139fc1ff97fc40ebe2afd956
Mauro Almiron <mdavid.almiron@gmail.com>
2024-11-19 11:25:14 -0300

    add comments

--- spec/models/province_spec.rb
+++ spec/models/province_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
+# Ayuda memoria
+
+# RSpec.describe es un grupo de ejemplo que describe que estamos testeando (en nuestro caso el modelo Province) y el comportamiento
+# que deseamos y como debe usarse
+
+# SPEC es la abreviacion de specification, especificamos el comportamiento deseado de un fragmento de codigo 
+
+# El test valida que el fragmento de codigo funciona correctamente.
+
 RSpec.describe Province, type: :model do
   subject { build(:province) }
   describe "validations" do
