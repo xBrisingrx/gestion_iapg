ca4c4229de914c5c87a75975383c2e2249a4d983
Mauro Almiron <mdavid.almiron@gmail.com>
2024-11-21 20:36:58 -0300

    🗑️remove white spaces

--- app/models/course_type.rb
+++ app/models/course_type.rb
@@ -2,13 +2,13 @@ class CourseType < ApplicationRecord
   belongs_to :room
   has_many :course_type_units
   has_many :units, through: :course_type_units
-  
+
   validates :description, :min_quota, :max_quota, :min_score, :max_score, :passing_score, :number_of_repeat, :fleet, :category, presence: true
   validates :min_quota, :max_quota, :min_score, :max_score, :passing_score, :number_of_repeat, numericality: { only_integer: true }
-  validates :name, 
+  validates :name,
     presence: true,
-    uniqueness: { message: "Este tipo de curso ya se encuentra registado."}
-    
+    uniqueness: { message: "Este tipo de curso ya se encuentra registado." }
+
   scope :actives, -> { where(active: true) }
 
   enum :fleet, [ :light, :heavy ] # discriminamos si el tipo de curso es de flota liviana o pesada
