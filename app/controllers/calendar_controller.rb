ddf7c8d783ee957f25a6ea1b9754b7b987d8f4f7
Mauro Almiron <mdavid.almiron@gmail.com>
2025-01-07 11:16:15 -0300

    translate days and months

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,6 +1,7 @@
 class CalendarController < ApplicationController
   def month
     @date = Date.parse(params.fetch(:date, Date.today.to_s))
+    @weekdays = { "Sun" => "Dom", "Mon" => "Lun", "Tue" => "Mar", "Wed" => "Mier", "Thu" => "Jue", "Fri" => "Sab", "Sat" => "Dom" }
     @courses = Course.where(from_date: @date.all_month).group_by { |c| c.from_date.to_date }
   end
 end
