13ca263a7baebf40a10a52e90a2966cc6f300aca
Mauro Almiron <mdavid.almiron@gmail.com>
2025-10-17 09:02:19 -0300

    mejorando la vista de turnos y horarios

--- app/javascript/controllers/forms/turn_controller.js
+++ app/javascript/controllers/forms/turn_controller.js
@@ -3,12 +3,23 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="forms--turn"
 export default class extends Controller {
   static targets = ["form", "changeTurn"]
+
+  connect() {
+    console.info("form turns")
+  }
+
+  abc() {
+    // console.info("conectado");
+  }
+
   update_turn() {
-    this.formTarget.requestSubmit()
+    this.formTarget.requestSubmit();
   }
 
   change_turn() {
     console.info("turnitos")
-    this.changeTurnTarget.requestSubmit()
+    this.changeTurnTarget.requestSubmit();
   }
+
+  
 }
