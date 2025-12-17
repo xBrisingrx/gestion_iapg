3dba64d13e4e3f3564cadb7d8223c4b1c4ee6b9b
Brisingr <mdavid.almiron@gmail.com>
2025-12-17 11:36:03 -0300

    fix carga de meses por tipo usuario

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -10,5 +10,10 @@ class CalendarController < ApplicationController
     @date = Date.parse(params.fetch(:date, Date.today.to_s))
     @weekdays = { "Sun" => "Dom", "Mon" => "Lun", "Tue" => "Mar", "Wed" => "Mier", "Thu" => "Jue", "Fri" => "Vier", "Sat" => "Sab" }
     @courses = Course.where(from_date: @date.all_month).group_by { |c| c.from_date.to_date }
+    if current_user.admin?
+      render :admin
+    else
+      render :clients
+    end
   end
 end
