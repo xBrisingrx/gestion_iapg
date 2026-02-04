9929f5b809b7a6e53fe0a5c32be17cf84e617079
Mauro Almiron <mdavid.almiron@gmail.com>
2026-02-04 18:47:45 -0300

    change: we filter data by active:true

--- app/controllers/course_people_controller.rb
+++ app/controllers/course_people_controller.rb
@@ -4,9 +4,9 @@ class CoursePeopleController < ApplicationController
   # GET /courses or /courses.json
   def index
     if current_user.admin?
-      @query = CoursePerson.by_course(params[:course_id])
+      @query = CoursePerson.actives.by_course(params[:course_id])
     else
-      @query = CoursePerson.by_course_and_company(params[:course_id], current_user.company_id)
+      @query = CoursePerson.actives.by_course_and_company(params[:course_id], current_user.company_id)
       # debugger
     end
     @course = Course.find(params[:course_id])
@@ -112,7 +112,7 @@ class CoursePeopleController < ApplicationController
   end
 
   def by_course
-    @course_people = CoursePerson.by_course(params[:course_id])
+    @course_people = CoursePerson.actives.by_course(params[:course_id])
   end
 
   def show_survey
