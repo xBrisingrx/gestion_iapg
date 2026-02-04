c2231c6255b57ff794fa1ebab0e67fcf55e2db62
Mauro Almiron <mdavid.almiron@gmail.com>
2026-02-04 18:59:33 -0300

    para los teoricos necesitamos ordenar por apellido xq no tienen hora

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -247,7 +247,8 @@ class CoursesController < ApplicationController
 
   def people_registered # descargamos un PDF con el listado de gente anotada en el curso
     course = Course.find(params[:id])
-    course_people = course.course_people.actives.joins(:unit).where(units: { category: params[:category] }).order(from_hour: :asc)
+    order_by_column = (params[:category] == "Teorico") ? "last_name" : "from_hour"
+    course_people = course.course_people.actives.joins(:unit).joins(:person).where(units: { category: params[:category] }).order("#{order_by_column} ASC")
     pdf = ListadoPdf.new(course, course_people)
     send_data pdf.render,
             filename: "asistencia_#{params[:category]}.pdf".downcase,
