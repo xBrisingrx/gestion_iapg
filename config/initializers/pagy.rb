627a4f0abedf67cd410134120a6e9f7620970bd1
Brisingr <mdavid.almiron@gmail.com>
2026-06-10 22:24:24 -0300

    limito los items que me trae pagy

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -2,7 +2,7 @@
 
 # Pagy initializer file (9.0.8)
 Pagy::DEFAULT[:items]       = 10                    # default
-
+Pagy::DEFAULT[:limit]       = 10
 # Frontend Extras
 # Bootstrap extra: Add nav, nav_js and combo_nav_js helpers and templates for Bootstrap pagination
 # See https://ddnexus.github.io/pagy/docs/extras/bootstrap
